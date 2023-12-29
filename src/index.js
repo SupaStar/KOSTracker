b0f24e700143e6eb7135140df8ff29c0d9e83d7b
Obed Noe Martinez Gonzalez <68682799+SupaStar@users.noreply.github.com>
2023-12-29 13:36:44 -0600

    Push inicial

---
+++ src/index.js
@@ -0,0 +1,90 @@
+const config = require('./variables');
+const { MongoClient } = require('mongodb');
+const puppeteer = require('puppeteer');
+const Agenda = require('agenda');
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+
+const mongoConexion = config.mongoConexion;
+const tokenTelegram = config.tokenTelegram;
+
+const agenda = new Agenda({ db: { address: mongoConexion } });
+const client = new MongoClient(mongoConexion);
+const bot = new TelegramBot(tokenTelegram, { polling: false }); // Configura el bot con polling desactivado
+
+const delay = (milliseconds) => new Promise((resolve) => setTimeout(resolve, milliseconds));
+
+var botMessage = "";
+// agenda.define("comprobarExistencias", async (job) => {
+//     ejecucion();
+// });
+
+// (async function () {
+//     await agenda.start();
+//     console.log("Bot iniciado con exito");
+//     await agenda.every("60 minutes", "comprobarExistencias");
+// })();
+
+ejecucion();
+
+async function ejecucion() {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: true });
+    const page = await browser.newPage();
+    page.goto('https://www.kosbeauty.mx/tienda/?v=12fd81cd143d');
+    await delay(2000);
+    await page.waitForSelector('.products.elementor-grid.columns-4');
+    const productAvailability = await page.evaluate(() => {
+        const products = [];
+        // Seleccionar todos los elementos li dentro de la lista de productos
+        const productItems = document.querySelectorAll('.products.elementor-grid.columns-4 > li');
+
+        productItems.forEach((item) => {
+            // Verificar si el producto está agotado
+            const isOutOfStock = item.querySelector('.ast-shop-product-out-of-stock') !== null;
+
+            // Obtener el precio de cada producto
+            const priceElement = item.querySelector('.price > .woocommerce-Price-amount')?.textContent;
+            const name = item.querySelector('.woocommerce-loop-product__title')?.textContent;
+            if (priceElement) {
+                const price = priceElement;
+                products.push({ name, price, isOutOfStock });
+            }
+        });
+
+        return products;
+    });
+
+    var lipsticksAvaliable = [];
+    productAvailability.forEach((product) => {
+        if (product.name.includes('LIPSTICK') && product.isOutOfStock == false) {
+            lipsticksAvaliable.push(product);
+        }
+    });
+    try {
+        if (lipsticksAvaliable.length > 0) {
+            botMessage = "Hay existencia de los siguientes productos: \n";
+            lipsticksAvaliable.forEach((product) => {
+                botMessage += product.name + " " + product.price + "\n";
+            });
+            await enviarMensajes();
+        }
+        await browser.close();
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function enviarMensajes() {
+    const urlApi = `${config.botUrlMessages}`;
+    const response = await axios.get(urlApi);
+    const chats = response.data.result.map((message) => message.message.chat.id);
+    chats.forEach((chat) => {
+        try {
+            bot.sendMessage(chat, botMessage);
+            console.log('Mensaje enviado con éxito a Telegram');
+          } catch (error) {
+            console.error('Error al enviar el mensaje a Telegram:', error.message);
+          }
+    });
+}
---
+++ src/variables.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoConexion: 'mongodb+srv://obednoe22:obednoe1@cluster0.9w8oird.mongodb.net/?retryWrites=true&w=majority',
+    tokenTelegram: '6861580555:AAFSYBG4l7KCYqG4cLRmvgeKzPqCrg3lJHU',
+    botUrlMessages: 'https://api.telegram.org/bot6861580555:AAFSYBG4l7KCYqG4cLRmvgeKzPqCrg3lJHU/getUpdates',
+}
